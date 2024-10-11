c358b2cf7bb3b545433047da5880d1773784607d
MD Akthar raza <mdaraza621@gmail.com>
2024-10-11 19:00:51 +0530

    first commit

---
+++ WebForm1.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="webformCRUD.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ValidationSummary ID="vs" runat="server" ValidationGroup="abc"  ShowMessageBox="true" ShowSummary="false"/>
+            <table>
+                <tr>
+                    <td>Name :</td>
+                    <td>
+                        <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="rfv" runat="server" ControlToValidate="txtname" ValidationGroup="abc"  ErrorMessage="please enter your name" Display="None"></asp:RequiredFieldValidator>
+                   <asp:RegularExpressionValidator  ID="rev" runat="server"  ControlToValidate="txtname" ValidationGroup="abc" ErrorMessage="please enter name should be alphabetic" Display="None" ValidationExpression="^[a-zA-Z ]*$" ></asp:RegularExpressionValidator>
+                        </td>
+                </tr>
+                <tr>
+                    <td>State  :</td>
+                    <td>
+                        <asp:DropDownList ID="ddlstate" runat="server"></asp:DropDownList>
+                                                <asp:RequiredFieldValidator ID="rfvs" runat="server" ValidationGroup="abc" ControlToValidate="rblgender" ErrorMessage="please select your state!!" Display="None"></asp:RequiredFieldValidator>
+
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>City  :</td>
+                    <td>
+                        <asp:DropDownList ID="ddlcity" runat="server"></asp:DropDownList></td>
+                </tr>
+                <tr>
+                    <td>Gender  :</td>
+                    <td>
+                        <asp:RadioButtonList ID="rblgender" runat="server" RepeatColumns="3"></asp:RadioButtonList>
+                        <asp:RequiredFieldValidator ID="rfvg" runat="server" ValidationGroup="abc" ControlToValidate="rblgender" ErrorMessage="please choose your gender!!" Display="None"></asp:RequiredFieldValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnsubmit" runat="server" Text="Submit" ValidationGroup="abc" OnClick="btnsubmit_Click" /></td>
+                    
+                </tr >
+
+                <tr >
+                    <td></td>
+                    <td style="background-color: slateblue; color: white">
+                        <asp:GridView ID="gvregister" runat="server" OnRowCommand="gvregister_RowCommand" AutoGenerateColumns="false">
+                            <Columns>
+                                <asp:TemplateField HeaderText="ID">
+                                    <ItemTemplate>
+                                        <%#Eval("id") %>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+
+                                <asp:TemplateField HeaderText="Name">
+                                    <ItemTemplate>
+                                        <%#Eval("name") %>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="State">
+                                    <ItemTemplate>
+                                        <%#Eval("sname") %>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="City">
+                                    <ItemTemplate>
+                                        <%#Eval("cname") %>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="Gender">
+                                    <ItemTemplate>
+                                        <%#Eval("gname") %>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+
+                                 <asp:TemplateField >
+     <ItemTemplate >
+          <asp:Button ID="btndelete" runat="server" Text="Delete" CommandName="D" CommandArgument=' <%#Eval("id") %>'  /></td>
+
+     </ItemTemplate>
+ </asp:TemplateField>
+                                                                <asp:TemplateField >
+    <ItemTemplate>
+         <asp:Button ID="btndedit" runat="server" Text="Edit" CommandName="E" CommandArgument=' <%#Eval("id") %>' /></td>
+
+    </ItemTemplate>
+</asp:TemplateField>
+
+
+                            </Columns>
+                        </asp:GridView>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+namespace webformCRUD
+{
+    public partial class WebForm1 : System.Web.UI.Page
+
+    {
+        SqlConnection con = new SqlConnection("data source = DESKTOP-3278F0B\\SQLEXPRESS; initial catalog =ddgproc; integrated security=true");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindState();
+                BindCity();
+                BindGender();
+                Show();
+            }
+
+
+        }
+
+       public void Clear()
+
+        {
+            txtname.Text = "";
+            ddlstate.SelectedValue = "0";
+            ddlcity.SelectedValue = "0";
+            rblgender.ClearSelection();
+            btnsubmit.Text = "Submit";
+        }
+        
+        public void Show()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("register_proc", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@action", "select");
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            
+            gvregister.DataSource = dt;
+            gvregister.DataBind();
+           
+            
+        }
+
+        public void BindState()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("state_get", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddlstate.DataValueField = "sid";
+            ddlstate.DataTextField = "sname";
+            ddlstate.DataSource = dt;
+            ddlstate.DataBind();
+            ddlstate.Items.Insert(0, new ListItem("--select--","0"));
+            
+        }
+
+        public void BindCity()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("city_get", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+          
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddlcity.DataValueField = "cid";
+            ddlcity.DataTextField = "cname";
+            ddlcity.DataSource = dt;
+            ddlcity.DataBind();
+            ddlcity.Items.Insert(0, new ListItem("--select--","0"));
+                
+        }
+        public void BindGender()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("gender_get", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            rblgender.DataValueField = "gid";
+            rblgender.DataTextField = "gname";
+            rblgender.DataSource = dt;
+            rblgender.DataBind();
+            
+
+        }
+
+
+
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+
+
+            if (btnsubmit.Text == "Submit")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("register_proc", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "insert");
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                cmd.Parameters.AddWithValue("@state", ddlstate.SelectedValue);
+                cmd.Parameters.AddWithValue("@city", ddlcity.SelectedValue);
+                cmd.Parameters.AddWithValue("@gender", rblgender.SelectedValue);
+
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Show();
+                Clear();
+
+
+            }
+
+            else if (btnsubmit.Text == "Update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("register_proc", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "update");
+                cmd.Parameters.AddWithValue("@name",txtname.Text);
+                cmd.Parameters.AddWithValue("@state", ddlstate.SelectedValue);
+                cmd.Parameters.AddWithValue("@city",ddlcity.SelectedValue);
+                cmd.Parameters.AddWithValue("@gender", rblgender.SelectedValue);
+                cmd.Parameters.AddWithValue("@id", ViewState["abc"]);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Show();
+                Clear();
+
+
+
+            }
+        }
+
+        protected void gvregister_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "D")
+            {
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand("register_proc", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "Delete");
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Show();
+
+            }
+            else if (e.CommandName == "E")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("register_proc", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "Edit");
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                con.Close();
+                txtname.Text = dt.Rows[0]["name"].ToString();
+                ddlstate.SelectedValue = dt.Rows[0]["state"].ToString();
+                ddlcity.SelectedValue = dt.Rows[0]["city"].ToString();
+                rblgender.SelectedValue = dt.Rows[0]["gender"].ToString();
+                btnsubmit.Text = "Update";
+                ViewState["abc"] = e.CommandArgument;
+            }
+
+        }
+    }
+}
